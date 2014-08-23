082131a1e065be058f636fd114e2e84a3fca5161
Novlize <Novlize@Novlizeui-MacBook-Pro.local>
2014-08-24 04:23:07 +0900

    complete mainpage without dropdown menu

--- js/shareController.js
+++ js/shareController.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-    $('#mainImageSlide').carousel({ interval: 5000 });
-    $(this).find('[data-bg-img]').each( function () {
-        $(this).css( {
-            'background-image' : 'url('+$(this).data('bg-img')+')',
-            'background-position' : 'center bottom',
-            'background-repeat':'no-repeat'
-        });
-    });
+	$('#mainImageSlide').carousel({ interval: 5000 });
+	$(this).find('[data-bg-img]').each( function () {
+		$(this).css( {
+			'background-image' : 'url('+$(this).data('bg-img')+')',
+			'background-position' : 'center bottom',
+			'background-repeat':'no-repeat'
+		});
+	});
+
+	$('.dropdown').dropdown();
+	$(document).click(function (e) {
+		//check if the clicked area is dropDown or not
+		if ($(".dropdown").has(e.target).length === 0) {
+			hide_dropdown();
+		}
+	});
 });
+
+
+function hide_dropdown() {
+	$('.dropDownData').hide();
+}
