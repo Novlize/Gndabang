718d3469756b9a7f2389b350d5e0dfe8e0066798
Novlize <Novlize@Novlizeui-MacBook-Pro.local>
2014-08-24 00:57:59 +0900

    main section done by 80%

--- js/shareController.js
+++ js/shareController.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     $('#mainImageSlide').carousel({ interval: 5000 });
+    $(this).find('[data-bg-img]').each( function () {
+        $(this).css( {
+            'background-image' : 'url('+$(this).data('bg-img')+')',
+            'background-position' : 'center bottom',
+            'background-repeat':'no-repeat'
+        });
+    });
 });
