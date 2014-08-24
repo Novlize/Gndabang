1d06c09a00e2e37871a0542240a3eb54b2b3e8b4
Novlize <Novlize@Novlizeui-MacBook-Pro.local>
2014-08-24 11:27:54 +0900

    complete dropdown

--- js/shareController.js
+++ js/shareController.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+
+	//========================
+	// Image Slider
+	//========================
 	$('#mainImageSlide').carousel({ interval: 5000 });
+	
+
+	//========================
+	// Background Selector
+	//========================
 	$(this).find('[data-bg-img]').each( function () {
 		$(this).css( {
 			'background-image' : 'url('+$(this).data('bg-img')+')',
@@ -8,16 +17,38 @@ $(document).ready(function() {
 		});
 	});
 
-	$('.dropdown').dropdown();
+
+	//========================
+	// DropDown Menu
+	//========================
+	$(this).find('.dropDown').each(function() {
+		$(this).find('> p.dropDownVal').click(function (e) {
+			hide_dropdown();
+			var $value = $(this);
+			var $menu = $(this).parent().find('> div.dropDownData');
+			if( $menu.css('display') == 'none' ) {
+				$menu.css('display', 'block');
+			}
+			$menu.find('> ul > li').each(function (e) {
+				var $data = $(this).html();
+				$(this).click(function (e) {
+					$value.html($data);
+					hide_dropdown();
+				});
+			});
+		});
+	});
 	$(document).click(function (e) {
 		//check if the clicked area is dropDown or not
-		if ($(".dropdown").has(e.target).length === 0) {
+		if ($(".dropDown").has(e.target).length == 0) {
 			hide_dropdown();
 		}
 	});
+
+
 });
 
-
+// Function to hide dropdown menu
 function hide_dropdown() {
 	$('.dropDownData').hide();
 }
